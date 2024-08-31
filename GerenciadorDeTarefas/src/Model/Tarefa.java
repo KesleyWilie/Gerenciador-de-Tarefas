f5b041f2935e372ea6dc2d7cee1834e11a0db00b
vinPedro <159270159+vinPedro@users.noreply.github.com>
2024-08-31 11:25:00 -0300

    feat: update Tarefa profile

--- GerenciadorDeTarefas/src/Model/Tarefa.java
+++ GerenciadorDeTarefas/src/Model/Tarefa.java
@@ -7,6 +7,7 @@ public class Tarefa implements EstadoDaTarefa, PrototypeTarefa{
     private Prioridade prioridade;
     private Estado concluida;
     
+    //TODO Contrutor da Tarefa
     public Tarefa(TarefaDTO tarefa) {
     	this.id = tarefa.getId();
         this.titulo = tarefa.getTitulo();
@@ -14,20 +15,6 @@ public class Tarefa implements EstadoDaTarefa, PrototypeTarefa{
         this.prioridade = tarefa.getPrioridade();
         this.concluida = tarefa.isConcluida();
     }
-    
-
-    //Usariamos esse construtor se o padrao Builder fosse aplicado na classe Tarefa.
-    /*private Tarefa(TarefaBuilder builder) {
-        this.id = builder.id;
-        this.titulo = builder.titulo;
-        this.descricao = builder.descricao;
-        this.prioridade = builder.prioridade;
-        this.concluida = builder.concluida;
-    }*/
-
-    /*public Tarefa(String titulo, String descricao, Prioridade prioridade) {
-        this(0, titulo, descricao, prioridade, false);
-    }*/
 
     // Getters e Setters Tarefa
     public int getId() { return id; }
@@ -43,6 +30,7 @@ public class Tarefa implements EstadoDaTarefa, PrototypeTarefa{
     public void setPrioridade(Prioridade prioridade) { this.prioridade = prioridade; }
     public void setConcluida(Estado concluida) { this.concluida = concluida; }
 
+    //TODO Metodo para mudar o estado da tarefa, utilizando do seu estado atual
 	@Override
 	public void mudarEstado() {
 		if(concluida.equals(Estado.Em_Andamento)) {
@@ -54,53 +42,11 @@ public class Tarefa implements EstadoDaTarefa, PrototypeTarefa{
 		
 	}
 
+	//TODO Implementação do Metodo para clonar uma tarefa
 	@Override
 	public PrototypeTarefa clonar(TarefaDTO tarefa) {
 		return new Tarefa(tarefa);
 	}
     
-    //Padrao builder na Tarefa
-    /*public static class TarefaBuilder {
-    	
-    	private int id;
-        private String titulo;
-        private String descricao;
-        private Prioridade prioridade;
-        private boolean concluida;
-        
-        //Métoddos builder para definir as propriedades da Tarefa
-        
-        public TarefaBuilder setId(int id) {
-            this.id = id;
-            return this;
-        }
-
-        public TarefaBuilder setTitulo(String titulo) {
-            this.titulo = titulo;
-            return this;
-        }
-
-        public TarefaBuilder setDescricao(String descricao) {
-            this.descricao = descricao;
-            return this;
-        }
-
-        public TarefaBuilder setPrioridade(Prioridade prioridade) {
-            this.prioridade = prioridade;
-            return this;
-        }
-
-        public TarefaBuilder setConcluida(boolean concluida) {
-            this.concluida = concluida;
-            return this;
-        }
-
-        // Método build para criar a instância de Tarefa
-        public Tarefa build() {
-            return new Tarefa(this);
-        }
-
-    }*/
-
     
 }
